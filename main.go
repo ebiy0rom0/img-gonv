9642a4eb4e672d4bde79ef40ea4f5e7519e03adc
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-01-13 02:32:18 +0900

    [add]convert parameter input

--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
 )
 
 var (
-	target       string
-	px, py, w, h uint
+	target string
+	px, py uint
+	w, h   int
 )
 
 func init() {
 	flag.StringVar(&target, "target", "", "convert image file or directory")
 	flag.UintVar(&px, "px", math.MaxUint, "reference point on horizontal axis")
 	flag.UintVar(&py, "py", math.MaxUint, "reference point on vertical axis")
-	flag.UintVar(&w, "w", math.MaxUint, "horizontal distance from px")
-	flag.UintVar(&h, "h", math.MaxUint, "vertical distance from py")
+	flag.IntVar(&w, "w", math.MaxInt, "horizontal distance from px")
+	flag.IntVar(&h, "h", math.MaxInt, "vertical distance from py")
 }
 
 func main() {
@@ -24,14 +25,13 @@ func main() {
 	if err := run(); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Scanln()
 }
 
 func run() error {
 	for len(target) == 0 {
 		fmt.Println("target is empty.")
 		fmt.Println("Please input convert image file or directory.")
-		fmt.Scan(&target)
+		fmt.Scanln(&target)
 	}
 	img, err := LoadImage(target)
 	if err != nil {
@@ -39,6 +39,32 @@ func run() error {
 	}
 	rect := img.Bounds()
 
-	fmt.Println(rect)
+	for px < uint(rect.Min.X) || px > uint(rect.Max.X) {
+		fmt.Println("x-pos exceeds image size.")
+		fmt.Printf("Please input x-pos.(Range: %d - %d)\n", rect.Min.X, rect.Max.X)
+		fmt.Scanln(&px)
+	}
+
+	for py < uint(rect.Min.Y) || py > uint(rect.Max.Y) {
+		fmt.Println("y-pos exceeds image size.")
+		fmt.Printf("Please input y-pos.(Range: %d - %d)\n", rect.Min.Y, rect.Max.Y)
+		fmt.Scanln(&py)
+	}
+
+	low, high := -int(px), rect.Max.X-int(px)
+	for w < low || w > high {
+		fmt.Println("horizontal distance exceeds image size.")
+		fmt.Printf("Please input horizontal distance.(Range: %d - %d)\n", low, high)
+		fmt.Scanln(&w)
+	}
+
+	low, high = -int(py), rect.Max.Y-int(py)
+	for h < low || h > high {
+		fmt.Println("vertical distance exceeds image size.")
+		fmt.Printf("Please input vertical distance.(Range: %d - %d)\n", low, high)
+		fmt.Scanln(&h)
+	}
+
+	fmt.Println(target, px, py, w, h)
 	return nil
 }
