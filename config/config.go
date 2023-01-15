17cd75299e98f8c99ed00a92819a5a3068b1e9ce
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-01-16 06:48:16 +0900

    [add]make out path
    is not exist make directory
    fix output path

--- config/config.go
+++ config/config.go
@@ -17,5 +17,11 @@ func init() {
 	if err := godotenv.Load(envfile); err != nil {
 		log.Fatal(err)
 	}
+
 	OutputPath = os.Getenv("OUTPUT_PATH")
+	if _, err := os.Stat(OutputPath); os.IsNotExist(err) {
+		if err := os.Mkdir(OutputPath, 0766); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
